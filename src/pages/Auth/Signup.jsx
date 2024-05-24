7b878627170bc59e1ededc51f71b0fa5639473b1
Gautam Magar <puskarmagar687@gmail.com>
2024-05-25 02:09:12 +0545

    adding password validation

--- src/pages/Auth/Signup.jsx
+++ src/pages/Auth/Signup.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import { Steeper_data } from "../../steeper_data";
-
 
 const Signup = ({}) => {
 
@@ -10,6 +8,9 @@ const Signup = ({}) => {
     email: "",
     password: "",
   });
+  const [letter_pass_status, setletter_pass_status] = useState(false);
+  const [numSp_pass_status, setnumSp_status] = useState(false);
+  const [char_pass_status, setchar_pass_status] = useState(false);
 
   const Signup_Steeper = [
     {
@@ -29,7 +30,7 @@ const Signup = ({}) => {
       p: "",
     },
     {
-      id: "about",
+      id: "name",
       header: "Tell us about yourself  ",
       title: "Name      ",
       p: "This name will appear on your profile      ",
@@ -41,7 +42,7 @@ const Signup = ({}) => {
     e.preventDefault();
     const Steper_Count = document.getElementById("Count_steeper");
     const Back = document.getElementById("back_btn");
- 
+
     if (form_index <= 3) {
       setform_index(form_index + 1);
     }
@@ -53,10 +54,10 @@ const Signup = ({}) => {
       Back.classList.remove("hidden");
       Back.classList.add("block");
     }
-    console.log(User_details[Signup_Steeperform_index].id);
   };
   const handler_Back = (e) => {
     e.preventDefault();
+    console.log(Signup_Steeper[form_index].id);
 
     if (form_index > 0) {
       setform_index(form_index - 1);
@@ -71,7 +72,6 @@ const Signup = ({}) => {
   }
   const form_handler = (e) => {
     e.preventDefault();
-
     const UserDetails = {
       Email: User_details.email,
       password: User_details.password,
@@ -82,15 +82,54 @@ const Signup = ({}) => {
   };
 
   const handler_input = (e) => {
+    const regex = /([A-z])/g;
+    const num_regex = /([0-9])/g;
+    const special_Char = /([$&+,:;=?@#|'<>.^*()%!-])/g;
+    const password_Valid = document.getElementById("password_validation");
+    const password_letter = document.getElementById("pass_letter");
+    const password_number_char = document.getElementById("pass_num_sp");
+    const password_charcter = document.getElementById("pass_char");
     const id = e.target.id;
     const value = e.target.value;
-
     const copy_User_details = { ...User_details };
     copy_User_details[id] = value;
-
+   
+    if (id === "password") {
+      password_Valid.classList.add("block");
+      password_Valid.classList.remove("hidden");
+      if (regex.test(value)) {
+        setletter_pass_status(true);
+        password_letter.classList.add("text-green-500");
+        password_letter.classList.remove("text-red-500");
+      } else {
+        setletter_pass_status(false);
+        password_letter.classList.remove("text-green-500");
+        password_letter.classList.add("text-red-500");
+      }
+      if (num_regex.test(value) || special_Char.test(value)) {
+        setnumSp_status(true);
+        password_number_char.classList.add("text-green-500");
+        password_number_char.classList.remove("text-red-500");
+      } else {
+        setnumSp_status(false);
+        password_number_char.classList.remove("text-green-500");
+        password_number_char.classList.add("text-red-500");
+      }
+      if (value.length >= 10) {
+        setchar_pass_status(true);
+        password_charcter.classList.add("text-green-500");
+        password_charcter.classList.remove("text-red-500");
+      } else {
+        setchar_pass_status(false);
+        password_charcter.classList.remove("text-green-500");
+        password_charcter.classList.add("text-red-500");
+      }
+    } else {
+      password_Valid.classList.add("hidden");
+      password_Valid.classList.remove("block");
+    }
     setUser_details(copy_User_details);
   };
-
 
   return (
     <div className="signup_container">
@@ -113,7 +152,7 @@ const Signup = ({}) => {
             <div className="form_container mt-6 w-full">
               <div className="back hidden" id="back_btn">
                 {form_index > 0 ? (
-                  <button className="" onClick={handler_Back}>
+                  <button className=" hover:border-none" onClick={handler_Back}>
                     {" "}
                     Back
                   </button>
@@ -121,8 +160,15 @@ const Signup = ({}) => {
                   <></>
                 )}
               </div>
-              <div className="user_form " id="form_handler">
-                <div className="form_title hidden" id="Count_steeper">
+              <div className="user_form mt-4" id="form_handler">
+                <div
+                  className={`   ${
+                    Signup_Steeper[form_index].id === "email"
+                      ? "form_title hidden"
+                      : "form_title block"
+                  } `}
+                  id="Count_steeper"
+                >
                   <h2 className="text-xl font-semibold text-gray-500 ">{`Step ${
                     form_index + 1
                   } of 3`}</h2>
@@ -146,11 +192,37 @@ const Signup = ({}) => {
                   placeholder={Signup_Steeper[form_index].placeholder}
                   className="rounded-md border-0 \text-gray-900 p-3 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 w-full mt-3 text-lg"
                   onChange={handler_input}
-                  value={User_details[Signup_Steeper[form_index].id]}
+                  value={User_details[Signup_Steeper[form_index].id] || ""}
                 />
               </div>
 
-              {form_index === Steeper_data.length - 1 ? (
+              <div
+                className={`${
+                  Signup_Steeper[form_index].id === "password"
+                    ? "password_validation block"
+                    : "password_validation hidden"
+                }`}
+                id="password_validation"
+              >
+                <h5 className="mt-4 sm:text-lg md:text-xl">
+                  Your password must contain at least
+                </h5>
+                <div className="list">
+                  <ul>
+                    <li className="letter text-red-500" id="pass_letter">
+                      1 letter
+                    </li>
+                    <li className="number text-red-500" id="pass_num_sp">
+                      1 number or special character (example: # ? ! &){" "}
+                    </li>
+                    <li className="char text-red-500" id="pass_char">
+                      10 characters
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              {form_index === Signup_Steeper.length - 1 ? (
                 <button
                   className="bg-blue-600 w-full md:w-3/4 mt-4 border-none focus:bg-blue-500 text-center md:mx-11"
                   onClick={form_handler}
@@ -158,14 +230,39 @@ const Signup = ({}) => {
                 >
                   Sign Up
                 </button>
+              ) : 
+              Signup_Steeper[form_index].id === "password" ? (
+                <>
+                  <button
+                    className={
+                      numSp_pass_status &&
+                      letter_pass_status &&
+                      char_pass_status
+                        ? "bg-blue-600 w-full md:w-3/4 mt-4 border-none focus:bg-blue-500 text-center md:mx-11"
+                        : "bg-stone-900 w-full md:w-3/4 mt-4 border-none focus:bg-blue-500 text-center md:mx-11 hover:cursor-help"
+                    }
+                    type="submit"
+                    disabled={
+                      !numSp_pass_status &&
+                      !letter_pass_status &&
+                      !char_pass_status
+                    }
+                  >
+                    {numSp_pass_status && letter_pass_status && char_pass_status
+                      ? "Next"
+                      : "loader"}
+                  </button>
+                </>
               ) : (
-                <button
-                  className="bg-blue-600 w-full md:w-3/4 mt-4 border-none focus:bg-blue-500 text-center md:mx-11"
-                  type="submit"
-                >
-                  {" "}
-                  Next
-                </button>
+                <>
+                  <button
+                    className="bg-blue-600 w-full md:w-3/4 mt-4 border-none focus:bg-blue-500 text-center md:mx-11"
+                    type="submit"
+                  >
+                    {" "}
+                    Next
+                  </button>
+                </>
               )}
             </div>
           </form>
